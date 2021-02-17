5874f544f4c1b81e0c3f9fa098e2c0df69dc8234
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-17 13:08:29 +0100

    seeded restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+10.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber, category: ["chinese", "italian", "japanese", "french", "belgian"].sample )
+  restaurant.save
+end
