73f2bc08f78f9100a97b156c3b5d91cffdded191
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-17 16:55:34 +0100

    complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
 
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review  = Review.new
+    # @restaurant = Restaurant.find(params[:restaurant_id])
+    # @review  = Review.new
   end
 
   def create
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
